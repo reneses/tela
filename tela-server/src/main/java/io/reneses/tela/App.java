d2f108c781f31f8b60bad3904397e9356c676867
Álvaro Reneses <alvaroreneses@gmail.com>
2016-10-28 03:08:16 +0200

    Programmatically configuration sample removed

--- tela-server/src/main/java/io/reneses/tela/App.java
+++ tela-server/src/main/java/io/reneses/tela/App.java
@@ -1,8 +1,6 @@
 package io.reneses.tela;
 
 import io.reneses.tela.core.api.server.TelaServer;
-import io.reneses.tela.core.configuration.Configuration;
-import io.reneses.tela.core.configuration.ConfigurationFactory;
 import io.reneses.tela.modules.instagram.InstagramTelaModule;
 import io.reneses.tela.modules.twitter.TwitterTelaModule;
 
@@ -15,12 +13,7 @@ public class App {
      */
     public static void main(String[] args) {
 
-        Configuration config = ConfigurationFactory.create();
-        config.setProperty(Configuration.Property.CACHE_TTL, 1000);
-        config.setProperty(Configuration.Property.PORT, 80);
-
         TelaServer tela = Assembler.build(
-                config,
                 new InstagramTelaModule(),
                 new TwitterTelaModule()
         );
