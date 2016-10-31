22b1ea3e0fd5526913217c304e0e9fa18624afd4
Álvaro Reneses <alvaroreneses@gmail.com>
2016-10-31 04:11:10 +0100

    Access token hidden from the scheduled response

--- tela-server/src/main/java/io/reneses/tela/core/scheduler/models/ScheduledAction.java
+++ tela-server/src/main/java/io/reneses/tela/core/scheduler/models/ScheduledAction.java
@@ -1,5 +1,6 @@
 package io.reneses.tela.core.scheduler.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -18,7 +19,7 @@ public class ScheduledAction {
     @JsonProperty("delay")
     private int delay;
 
-    @JsonProperty("accessToken")
+    @JsonIgnore
     private String accessToken;
 
     @JsonProperty("module")
